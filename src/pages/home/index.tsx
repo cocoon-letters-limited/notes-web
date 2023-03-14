a14bf36de82951b9f2d99613f6fb8a693310565f
muyi <eroosamuyi11451@gmail.com>
2023-03-14 12:09:46 +0100

    image fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,29 +2,24 @@
 /* eslint-disable prettier/prettier */
 
 import React from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import production1 from 'assets/Production1.png'
 
 const Home = () => {
   // eslint-disable-next-line react/self-closing-comp
   return <div>
-    <div className="bg-gray-400 h-48 flex items-center justify-center z-0">
-       <div>
-       <div>
-        <p>    Make your assets do much more in one place </p>
-        <p>The ability to build systems that resist, absorb, recover from, or adapt to an adverse occurrence during operation that may cause harm, destruction, or loss of the ability to perform mission-related functions.</p>
-      </div>
+    <div className="bg-gray-400 h-48 text-center z-0">
 
-
-       </div>  
-   
+      <p>    Make your assets do much more in one place </p>
+      <p>The ability to build systems that resist, absorb, recover from, or adapt to an adverse occurrence during operation that may cause harm, destruction, or loss of the ability to perform mission-related functions.</p>
 
     </div>
-   <img className="z-40 ml-4 mt-0 h-10 bg-yellow-400" src="./../../assets/images/Production 1.png" alt="product" sizes=""  />
+    <img className="z-40 ml-4 mt-0 h-10  bg-yellow-400" src={production1} alt="product" sizes="" />
 
-
-  <div className='text-center'>
-       <p className="text-4xl font-bold text-blue-600">Our Solution for your business</p>
-       <p>We make it easy for users to use our platform, that's why we provide this benefit.</p>
-  </div>
+    <div className='text-center'>
+      <p className="text-4xl font-bold text-blue-600">Our Solution for your business</p>
+      <p className="text-gray-400">We make it easy for users to use our platform, that's why we provide this benefit.</p>
+    </div>
   </div>;
 };
 
