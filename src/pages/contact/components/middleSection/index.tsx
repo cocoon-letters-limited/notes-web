29a688b69976252cee2ca7809e77cf904e7cf671
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-03-14 18:17:16 +0100

    fix: contact us page mobile responsiveness

--- src/pages/contact/components/middleSection/index.tsx
+++ src/pages/contact/components/middleSection/index.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const MiddleSection = () => {
   return (
     <section className="relative">
-      <div className="container px-4 lg:px-8 xl:px-4 mx-auto pt-20 md:pt-24 lg:pt-28 pb-32 lg:pb-40 relative">
+      <div className="container px-4 md:px-8 xl:px-4 mx-auto pt-20 md:pt-24 lg:pt-28 pb-32 lg:pb-40 relative">
         <div className="flex flex-row flex-wrap">
-          <div className="w-full md:w-6/12 lg:w-5/12">
+          <div className="w-full md:w-5/12">
             <p className="tracking-[.5em] text-sm-15 lg:text-base uppercase">
               Features
             </p>
@@ -13,7 +13,7 @@ const MiddleSection = () => {
               All in one place.
             </h1>
           </div>
-          <div className="pt-6 flex-1 md:pl-6 lg:pl-8 xl:pl-12">
+          <div className="pt-6 flex-1 md:pl-4 lg:pl-8 xl:pl-12">
             <p className="text-base leading-7 lg:text-lg lg:leading-8 xl:text-xl xl:leading-9 text-textColor">
               There are many variations of passages of Lorem Ipsum available,
               but majority have su alteration in some form, by injected humour,
--- src/pages/contact/components/topSection/index.tsx
+++ src/pages/contact/components/topSection/index.tsx
@@ -4,18 +4,18 @@ import style from "./style.module.css";
 const TopSection = () => {
   return (
     <section className="relative bg-[#3B69E6]">
-      <div className="container px-4 lg:px-8 xl:px-4 mx-auto pt-24 md:pt-28 lg:pt-32 pb-32 lg:pb-40 relative">
+      <div className="container px-4 md:px-8 xl:px-4 mx-auto pt-24 md:pt-28 lg:pt-32 pb-32 lg:pb-40 relative">
         <div className="flex flex-row flex-wrap justify-between">
           <div className="w-full md:w-5/12 flex flex-row items-start">
-            <div className="w-4 h-5 bg-[#FFC700]" />
-            <div className="pl-4 -mt-3 flex-1">
-              <h1 className="text-white text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
+            <div className="w-3 md:w-4 h-4 md:h-5 bg-[#FFC700]" />
+            <div className="pl-2 md:pl-4 -mt-1 md:-mt-3 flex-1">
+              <h1 className="text-white text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
                 Reach out to us
               </h1>
             </div>
           </div>
 
-          <div className="w-full md:w-6/12">
+          <div className="mt-8 md:mt-0 w-full md:w-6/12">
             <form className="w-full">
               <textarea
                 className={`w-full py-4 px-4 lg:py-6 lg:px-6 rounded-md placeholder:text-white text-white text-base lg:text-lg font-medium focus:outline-none ring-0 focus:ring-0 resize-none ${style.input}`}
