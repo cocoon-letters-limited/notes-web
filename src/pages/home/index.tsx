69d9ff1f026657e8a2071c8a86ce2d13f712cb5f
muyi <eroosamuyi11451@gmail.com>
2023-03-15 17:11:32 +0100

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,17 +6,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import production1 from 'assets/Production1.png'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import  rectangle1059 from 'assets/Rectangle1059.png';
+import production2 from 'assets/Production2.png'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import rectangle1059 from 'assets/Rectangle1059.png';
 
 const Home = () => {
   // eslint-disable-next-line react/self-closing-comp
   return <div>
-    <div className='bg-blue-100  ' >
+    <div className='b ' style={{ background: '#F1F5FF' }} >
       <div className=''>
         <div className="bg-blue-700 pb-48 pt-20 h-auto  text-center flex justify-center ">
-          <div className='w-2/4'>
-            <p className='font-bold text-6xl text-white mt-10'> Make your assets do much more in one place </p>
-            <p className='text-gray-300 mt-10 mb-10 '>The ability to build systems that resist, absorb, recover from, or adapt to an adverse occurrence during operation that may cause harm, destruction, or loss of the ability to perform mission-related functions.</p>
+          <div className=''>
+            <p className='font-bold text-6xl text-white mt-10'> Make your assets do <br /> much more in <span className='text-yellow-400'>  one place </span></p>
+            <p className='text-gray-300 mt-10 mb-10 '>The ability to build systems that resist, absorb, recover from, or adapt to an adverse <br /> occurrence during operation that may cause harm, destruction, or loss of the ability to <br />  perform mission-related functions.</p>
             <div className=''>
               <button className="bg-white hover:bg-blue-700 mr-3 text-blue-700  font-bold py-3 pr-8 pl-8 px-4 rounded">  Sales Enquiry</button>
               <button className="  font-bold py-2 px-4 rounded inline-flex items-center text-white">
@@ -32,30 +34,24 @@ const Home = () => {
 
 
         <div className='container mx-auto '>
-          <div className='m-10 p-10'>
+          <div className='m-10 p-20'>
 
             <button className="bg-white font-bold py-3  px-6  rounded">  Button</button>
 
 
             <div className="grid grid-cols-2 gap-4 mt-10">
               <div>
-                <h1 className='font-medium '>Tools</h1>
-                <h2 className='text-6xl  mt-3 mb-3 font-bold w-10'>Seamless integration</h2>
+                <h1 className='font-medium text-2xl'>Tools</h1>
+                <p className='text-6xl  mt-3 mb-3 font-semibold w-10'>Seamless integration</p>
                 <p className='w-4/5'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit..</p>
               </div>
-              <div className='grid grid-cols-4 '>
-                <ul className="items-center justify-center ">
-                  <li className="text-gray-900 font-bold">
-                     
-                  </li>
-                  <li className="text-gray-600 hover:text-blue-600">
-                    <p>About US</p>
-                  </li>
-                  <li className="text-gray-600 hover:text-blue-600">
-                    <p>Contact US</p>
-                  </li>
-                </ul>
-
+              <div className=''>
+                <div className='flex'>
+                  <button className=" rounded-full h-10 w-10 bg-white flex items-center justify-center">
+                    <svg className="fill-current w-4 h-4  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                  </button>
+                  <p className='m-2 ml-5'>Secure and encrypted integration</p>
+                </div>
               </div>
             </div>
           </div>
@@ -67,25 +63,26 @@ const Home = () => {
 
     <div className='text-center p-10'>
       <p className="text-4xl font-bold text-blue-600">Our Solution for your business</p>
-      <p className="text-gray-400">We make it easy for users to use our platform, that's why we provide this benefit.</p>
+      <p className="text-gray-400 m-5">We make it easy for users to use our platform, that's why we provide this benefit.</p>
     </div>
 
     <div className='container mx-auto mt-20'>
-      
+
+      <div>
         <div className="grid grid-cols-3  ">
           <div>
             <img className="bg-yellow-50 w-50  rounded" src={rectangle1059} alt="product" sizes="" />
           </div>
-     
+
           <div className='grid  '>
             <div className=" border-r-2  pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
-              <p className='text-4xl '>CMMS - Computerized Maintenance Management System </p>
+              <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
               <p className='text-gray-700'>Our Savings account is design to provides principal security and a modest interest...</p>
             </div>
             <div className=" border-r-2 pt-10 pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
-              <p className='text-4xl '>CMMS - Computerized Maintenance Management System </p>
+              <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
               <p className='text-gray-700'>Our Savings account is design to provides principal security and a modest interest...</p>
             </div>
           </div>
@@ -93,18 +90,104 @@ const Home = () => {
           <div className='grid  '>
             <div className="pl-10 pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
-              <p className='text-4xl '>CMMS - Computerized Maintenance Management System </p>
+              <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
               <p className='text-gray-700'>Our Savings account is design to provides principal security and a modest interest...</p>
             </div>
             <div className="p-10 pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
-              <p className='text-4xl '>CMMS - Computerized Maintenance Management System </p>
+              <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
               <p className='text-gray-700'>Our Savings account is design to provides principal security and a modest interest...</p>
             </div>
           </div>
         </div>
-    
+
+        <div className='text-center p-20 '>
+          <p className="text-4xl font-bold ">CMMS - ComputerizedMaintenance <br /> Management System </p>
+        </div>
+
+        <div className='grid grid-cols-5'>
+          <div className="max-w-sm text-center rounded overflow-hidden shadow-lg bg-orange-50" >
+            <div className="px-6 py-6">
+              <div className=" text-3xl mb-2 text-orange-300">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">Dashboard Process Map Plant Hierachy
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
+
+    <div className='mt-48'>
+      <div className='flex '>
+        <div className="w-1/1 pl-32 mb:p-0 container mx-auto" >
+          <p className='text-6xl text-blue-700  font-bold'>Easy collaborative <br /> Assets team</p>
+          <p className='mt-7 mb-7'>More than 10.000 people have tried</p>
+          <button className="bg-blue-700 hover:bg-blue-200 mr-3 text-white  font-bold py-3 pr-8 pl-8 px-4 rounded">  Try for free</button>
+          <div className='grid-cols-2 grid'>
+
+            <div className='mt-20 m-10'>
+              <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
+                <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+              </button>
+              <p className='text-3xl mt-5 mb-5'>
+                Instant <br />transactions
+              </p>
+              <p className='text-2xl text-gray-400'>Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.</p>
+            </div>
+            <div className='mt-20 m-10'>
+              <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
+                <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+              </button>
+              <p className='text-3xl mt-5 mb-5'>
+                Instant <br />transactions
+              </p>
+              <p className='text-2xl text-gray-400'>Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <img className=" w-1/3    rounded" src={production2} alt="product" sizes="" />
+
+      </div>
+    </div>
+
+    <div className='pt-20 mt-20 pb-20' style={{ background: ' #F1F5FF' }}>
+
+      <div className=" mb:p-0 container mx-auto" >
+        <div className='flex justify-between'>
+          <p className='text-5xl font-bold text-blue-600'>
+            NOTE is loved by users
+          </p>
+          <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
+            <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        <div className='grid grid-cols-3 gap-10 mt-10 '>
+        <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
+          <div className="px-6 py-6">
+            <div className="flex ">
+              <img className="rounded-full h-20 w-20" src={production2} alt="product" sizes="" />
+              <div className='ml-5 mt-3'>
+                <p className='text-2xl font-semibold'>Briana Patton</p>
+                <p className='text-slate-400'>Manager</p>
+              </div>
+            </div>
+            <p className="text-gray-400 mt-5 mb-5 text-xl">Sed mattis est eget penatibus mauris, sed condimentum vitae viverra. Ipsum ut aliquet et morbi ac in. Lacinia mattis eget nisl pellentesque non, porttitor. Vitae et vestibulum ac id. Dui aliquet porttitor libero consequat volutpat eget sed turpis. Feugiat maecenas commodo et morbi morbi gravida.
+            </p>
+
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <div className='text-center p-20 '>
+          <p className="text-4xl font-bold text-blue-700">Our pricing</p>
+        </div>
+
   </div>;
 };
 
