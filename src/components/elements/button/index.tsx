c5ea97cb6021098586feb92f9d1f92f35cfd1b88
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-03-16 12:06:02 +0100

    fix: update button styling on auth screen

--- src/components/elements/button/index.tsx
+++ src/components/elements/button/index.tsx
@@ -11,19 +11,19 @@ const ButtonGroup = ({ title, type, buttonStyleType, ...props }: Props) => {
   const buttonTypeClassName = () => {
     switch (buttonStyleType) {
       case "primary":
-        return "bg-primary text-white border-primary";
+        return "bg-primary text-white border-primary hover:bg-[#073CA1]";
       case "outline":
-        return "text-primary bg-white border-[#D8DDE7]";
+        return "text-primary bg-white border-[#D8DDE7] hover:border-primary";
 
       default:
-        return "bg-primary text-white border-primary";
+        return "bg-primary text-white border-primary hover:bg-[#073CA1]";
     }
   };
 
   return (
     <button
       type={type || "button"}
-      className={`mt-3 block px-5 h-14 lg:h-16 font-semibold text-sm md:text-sm-15 lg:text-base w-full rounded-md border-[2px] focus:outline-none ${buttonTypeClassName()}`}
+      className={`mt-3 block px-5 h-14 lg:h-16 font-semibold text-sm md:text-sm-15 lg:text-base w-full rounded-md border-[2px] focus:outline-none transition-all ease-in-out duration-200 ${buttonTypeClassName()}`}
       {...props}
     >
       {title}
