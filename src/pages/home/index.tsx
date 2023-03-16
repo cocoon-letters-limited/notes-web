8af9685887fb0880ff962fb07d125fb5fec111cc
muyi <eroosamuyi11451@gmail.com>
2023-03-16 17:29:44 +0100

    home fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable prettier/prettier */
@@ -12,8 +13,8 @@ import rectangle1059 from 'assets/Rectangle1059.png';
 
 const Home = () => {
   // eslint-disable-next-line react/self-closing-comp
-  return <div>
-    <div className='b ' style={{ background: '#F1F5FF' }} >
+  return <div className=''>
+    <div className='' style={{ background: '#F1F5FF' }} >
       <div className=''>
         <div className="bg-blue-700 pb-48 pt-20 h-auto  text-center flex justify-center ">
           <div className=''>
@@ -39,7 +40,7 @@ const Home = () => {
             <button className="bg-white font-bold py-3  px-6  rounded">  Button</button>
 
 
-            <div className="grid grid-cols-2 gap-4 mt-10">
+            <div className="md:grid md:grid-cols-2 gap-4 mt-10">
               <div>
                 <h1 className='font-medium text-2xl'>Tools</h1>
                 <p className='text-6xl  mt-3 mb-3 font-semibold w-10'>Seamless integration</p>
@@ -66,16 +67,16 @@ const Home = () => {
       <p className="text-gray-400 m-5">We make it easy for users to use our platform, that's why we provide this benefit.</p>
     </div>
 
-    <div className='container mx-auto mt-20'>
+    <div className='md:container md:mx-auto mt-20'>
 
       <div>
-        <div className="grid grid-cols-3  ">
+        <div className="md:grid md:grid-cols-3  ">
           <div>
             <img className="bg-yellow-50 w-50  rounded" src={rectangle1059} alt="product" sizes="" />
           </div>
 
-          <div className='grid  '>
-            <div className=" border-r-2  pb-5">
+          <div className='md:grid  '>
+            <div className=" md:border-r-2  pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
               <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
               <p className='text-gray-700'>Our Savings account is design to provides principal security and a modest interest...</p>
@@ -87,7 +88,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className='grid  '>
+          <div className='md:grid  '>
             <div className="pl-10 pb-5">
               <h5 className='text-6xl font-extrabold text-gray-200 '>01</h5>
               <p className='text-4xl pt-5 pb-5'>CMMS - Computerized Maintenance Management System </p>
@@ -105,7 +106,7 @@ const Home = () => {
           <p className="text-4xl font-bold ">CMMS - ComputerizedMaintenance <br /> Management System </p>
         </div>
 
-        <div className='grid grid-cols-5'>
+        <div className='md:grid grid-cols-5'>
           <div className="max-w-sm text-center rounded overflow-hidden shadow-lg bg-orange-50" >
             <div className="px-6 py-6">
               <div className=" text-3xl mb-2 text-orange-300">The Coldest Sunset</div>
@@ -120,14 +121,14 @@ const Home = () => {
     </div>
 
     <div className='mt-48'>
-      <div className='flex '>
-        <div className="w-1/1 pl-32 mb:p-0 container mx-auto" >
+      <div className='md:flex '>
+        <div className="md:w-1/1 md:pl-32 m-10 mb:p-0 md:container md:mx-auto" >
           <p className='text-6xl text-blue-700  font-bold'>Easy collaborative <br /> Assets team</p>
           <p className='mt-7 mb-7'>More than 10.000 people have tried</p>
           <button className="bg-blue-700 hover:bg-blue-200 mr-3 text-white  font-bold py-3 pr-8 pl-8 px-4 rounded">  Try for free</button>
-          <div className='grid-cols-2 grid'>
+          <div className='md:grid-cols-2 md:grid'>
 
-            <div className='mt-20 m-10'>
+            <div className='mt-20 md:m-10'>
               <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
                 <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
               </button>
@@ -136,7 +137,7 @@ const Home = () => {
               </p>
               <p className='text-2xl text-gray-400'>Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.</p>
             </div>
-            <div className='mt-20 m-10'>
+            <div className='mt-20 md:m-10'>
               <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
                 <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
               </button>
@@ -149,23 +150,23 @@ const Home = () => {
 
         </div>
 
-        <img className=" w-1/3    rounded" src={production2} alt="product" sizes="" />
+        <img className=" md:w-1/3    rounded" src={production2} alt="product" sizes="" />
 
       </div>
     </div>
 
     <div className='pt-20 mt-20 pb-20' style={{ background: ' #F1F5FF' }}>
 
-      <div className=" mb:p-0 container mx-auto" >
+      <div className=" mb:p-0 md:container md:mx-auto" >
         <div className='flex justify-between'>
           <p className='text-5xl font-bold text-blue-600'>
             NOTE is loved by users
           </p>
-          <button className=" rounded-full p-5  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
+          <button className=" rounded-full p-5 border-2 border-blue-700  flex text-blue-700 items-center justify-center" style={{ background: ' #EAEFF8' }}>
             <svg className="fill-current w-8 h-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
           </button>
         </div>
-        <div className='grid grid-cols-3 gap-10 mt-10 '>
+        <div className='md:grid grid-cols-3 gap-10 mt-10 '>
         <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
           <div className="px-6 py-6">
             <div className="flex ">
@@ -177,7 +178,32 @@ const Home = () => {
             </div>
             <p className="text-gray-400 mt-5 mb-5 text-xl">Sed mattis est eget penatibus mauris, sed condimentum vitae viverra. Ipsum ut aliquet et morbi ac in. Lacinia mattis eget nisl pellentesque non, porttitor. Vitae et vestibulum ac id. Dui aliquet porttitor libero consequat volutpat eget sed turpis. Feugiat maecenas commodo et morbi morbi gravida.
             </p>
-
+          </div>
+        </div>
+        <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
+          <div className="px-6 py-6">
+            <div className="flex ">
+              <img className="rounded-full h-20 w-20" src={production2} alt="product" sizes="" />
+              <div className='ml-5 mt-3'>
+                <p className='text-2xl font-semibold'>Briana Patton</p>
+                <p className='text-slate-400'>Manager</p>
+              </div>
+            </div>
+            <p className="text-gray-400 mt-5 mb-5 text-xl">Sed mattis est eget penatibus mauris, sed condimentum vitae viverra. Ipsum ut aliquet et morbi ac in. Lacinia mattis eget nisl pellentesque non, porttitor. Vitae et vestibulum ac id. Dui aliquet porttitor libero consequat volutpat eget sed turpis. Feugiat maecenas commodo et morbi morbi gravida.
+            </p>
+          </div>
+        </div>
+        <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
+          <div className="px-6 py-6">
+            <div className="flex ">
+              <img className="rounded-full h-20 w-20" src={production2} alt="product" sizes="" />
+              <div className='ml-5 mt-3'>
+                <p className='text-2xl font-semibold'>Briana Patton</p>
+                <p className='text-slate-400'>Manager</p>
+              </div>
+            </div>
+            <p className="text-gray-400 mt-5 mb-5 text-xl">Sed mattis est eget penatibus mauris, sed condimentum vitae viverra. Ipsum ut aliquet et morbi ac in. Lacinia mattis eget nisl pellentesque non, porttitor. Vitae et vestibulum ac id. Dui aliquet porttitor libero consequat volutpat eget sed turpis. Feugiat maecenas commodo et morbi morbi gravida.
+            </p>
           </div>
         </div>
         </div>
@@ -187,6 +213,73 @@ const Home = () => {
     <div className='text-center p-20 '>
           <p className="text-4xl font-bold text-blue-700">Our pricing</p>
         </div>
+
+    <div className='md:grid grid-cols-3 container mx-auto gap-10 mb-20'>
+    <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
+          <div className="px-6 py-6">
+              <div className='ml-5 mt-3 text-center'>
+                <p className='text-2xl font-semibold text-blue-700'>Standart</p>
+                
+                <p className='text-slate-400 m-5'>The national average cost of buying coin easy.</p>
+                
+                <p className='text-5xl font-semibold text-blue-700 m-5'>$5 <span  className='text-slate-400 text-xs'>/month</span></p>
+                
+                <button className="bg-blue-700  text-white w-64 py-3 pr-8 pl-8 px-4 rounded">Select Plan</button>
+            </div>
+
+            <div className='flex mt-5'>
+                  <button className=" rounded-full h-8 w-8 bg-blue-700 text-white flex items-center justify-center">
+                    <svg className="fill-current w-3 h-3  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                  </button>
+                  <p className='m-1 ml-5'>Secure and encrypted integration</p>
+                </div>
+          </div>
+    </div>
+
+        <div className="rounded-lg py-2 px-4 shadow-lg bg-blue-700 text-white" >
+          <div className="px-6 py-6">
+              <div className='ml-5 mt-3 text-center'>
+                <p className='text-2xl font-semibold text-white-700'>Standart</p>
+                
+                <p className='text-slate-400 m-5'>The national average cost of buying coin easy.</p>
+                
+                <p className='text-5xl font-semibold text-white m-5'>$5 <span  className='text-slate-400 text-xs'>/month</span></p>
+                
+                <button className="bg-white  text-blue-700 w-64 py-3 pr-8 pl-8 px-4 rounded">Select Plan</button>
+            </div>
+
+            <div className='flex mt-5'>
+                  <button className=" rounded-full h-8 w-8 text-blue-700 bg-white flex items-center justify-center">
+                    <svg className="fill-current w-3 h-3  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                  </button>
+                  <p className='m-1 ml-5'>Secure and encrypted integration</p>
+                </div>
+
+                
+          </div>
+        </div>
+
+        <div className="rounded-lg py-2 px-4 shadow-lg bg-white" >
+          <div className="px-6 py-6">
+              <div className='ml-5 mt-3 text-center'>
+                <p className='text-2xl font-semibold text-blue-700'>Standart</p>
+                
+                <p className='text-slate-400 m-5'>The national average cost of buying coin easy.</p>
+                
+                <p className='text-5xl font-semibold text-blue-700 m-5'>$5 <span  className='text-slate-400 text-xs'>/month</span></p>
+                
+                <button className="bg-blue-700  text-white w-64 py-3 pr-8 pl-8 px-4 rounded">Select Plan</button>
+            </div>
+
+            <div className='flex mt-5'>
+                  <button className=" rounded-full h-8 w-8 bg-blue-700 text-white flex items-center justify-center">
+                    <svg className="fill-current w-3 h-3  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                  </button>
+                  <p className='m-1 ml-5'>Secure and encrypted integration</p>
+                </div>
+          </div>
+    </div>
+    </div>
 
   </div>;
 };
