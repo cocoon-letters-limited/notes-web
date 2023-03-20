dea321590a26f5fcd7251b42c7af147b625d8380
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-03-21 00:32:14 +0100

    feat: added criticality summary section on dashboard

--- src/pages/dashboard/components/criticalSummarySection/components/equipmentCard/index.tsx
+++ src/pages/dashboard/components/criticalSummarySection/components/equipmentCard/index.tsx
@@ -1,9 +1,97 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
 const EquipmentCard = () => {
+  const data: any = {
+    series: [
+      {
+        name: "Turbine",
+        data: [28, 29, 33, 36, 32, 32, 33],
+      },
+      {
+        name: "Generator",
+        data: [32, 29, 30, 36, 34, 28, 20],
+      },
+      {
+        name: "Diesel Engine",
+        data: [34, 30, 27, 24, 30, 34, 36],
+      },
+      {
+        name: "CNC machine",
+        data: [33, 30, 26, 26, 28, 32, 36],
+      },
+      {
+        name: "Boom Lifts",
+        data: [31, 28, 24, 24, 28, 31, 34],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        stacked: false,
+        type: "area",
+        toolbar: {
+          show: false,
+        },
+        dropShadow: {
+          enabled: false,
+        },
+      },
+      colors: ["#F66716", "#16F62C", "#BC16F6", "#168BF6", "#FFD700"],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      title: {
+        text: "",
+      },
+      grid: {
+        borderColor: "#E4E4E4",
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+        title: {
+          text: "Month",
+        },
+      },
+      yaxis: {
+        title: {
+          text: "Equipment",
+        },
+        min: 5,
+        max: 40,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "right",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5,
+      },
+    },
+  };
+
   return (
     <div className="bg-[#F6F7FB] px-6 py-6 rounded-lg h-[27rem]">
       <h4 className="text-lg lg:text-xl text-[#224280] font-bold">Equipment</h4>
+
+      <div className="mt-1 overflow-auto">
+        <Chart
+          options={data.options}
+          series={data.series}
+          type="line"
+          height={350}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/dashboard/components/criticalSummarySection/components/materialCard/index.tsx
+++ src/pages/dashboard/components/criticalSummarySection/components/materialCard/index.tsx
@@ -1,9 +1,74 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
 const MaterialCard = () => {
+  const colors = [
+    "#06650F",
+    "#F9E42F",
+    "#16F62C",
+    "#F99C2F",
+    "#06650F",
+    "#D12F1F",
+  ];
+
+  const data: any = {
+    series: [
+      {
+        data: [21, 22, 10, 28, 16, 21],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "bar",
+        toolbar: {
+          show: false,
+        },
+      },
+      colors,
+      plotOptions: {
+        bar: {
+          columnWidth: "45%",
+          distributed: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      xaxis: {
+        categories: [
+          ["Lever"],
+          ["Metal", "plate"],
+          ["Clutch"],
+          "Bolt",
+          ["Battery"],
+          ["Plug"],
+        ],
+        labels: {
+          style: {
+            colors,
+            fontSize: "12px",
+          },
+        },
+      },
+    },
+  };
+
   return (
     <div className="bg-[#F6F7FB] px-6 py-6 rounded-lg h-[27rem]">
       <h4 className="text-lg lg:text-xl text-[#224280] font-bold">Material</h4>
+
+      <div className="mt-1 overflow-auto">
+        <Chart
+          options={data.options}
+          series={data.series}
+          type="bar"
+          height={350}
+        />
+      </div>
     </div>
   );
 };
