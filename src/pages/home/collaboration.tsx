c2d5171f6b606bd50b913cd867baef7bc6636d4d
muyi <eroosamuyi11451@gmail.com>
2023-04-01 13:04:22 +0100

    style update

--- src/pages/home/collaboration.tsx
+++ src/pages/home/collaboration.tsx
@@ -4,10 +4,10 @@ import production2 from "assets/Production2.png";
 function Collaboration() {
   return (
     <div>
-      <div className="mt-48">
+      <div className="p-10 md:p-0">
         <div className="md:flex ">
           <div className="md:w-1/1 md:pl-32 md:mr-32  mb:p-0 md:container md:mx-auto md:pr-20">
-            <p className="text-6xl text-blue-700  font-bold">
+            <p className="text-4xl text-blue-700 mt-48  font-bold">
               Save more, Make more, <br /> Be more{" "}
             </p>
             <div className="flex mt-5 mb-5">
@@ -30,11 +30,17 @@ function Collaboration() {
                   alt="product"
                   sizes=""
                 />
+                <img
+                  className="rounded-full  w-8 h-8  -ml-2"
+                  src={production2}
+                  alt="product"
+                  sizes=""
+                />
               </div>
-              <p className="md:ml-5 ">
+              <small className="md:ml-5 text-sm-15">
                 NoteOpX has helped more than 1,000 businesses eliminate waste
                 from their operational processes.
-              </p>
+              </small>
             </div>
             <button
               type="button"
@@ -43,7 +49,7 @@ function Collaboration() {
               {" "}
               Try for free
             </button>
-            <div className="md:grid-cols-2 md:grid">
+            <div className="md:grid-cols-2 md:grid ">
               <div className="mt-20 md:m-10">
                 <button
                   type="button"
@@ -65,10 +71,10 @@ function Collaboration() {
                     />
                   </svg>
                 </button>
-                <p className="text-3xl mt-5 mb-5">
+                <p className="text-2xl mt-5 mb-5">
                   Standardize operational safety processes
                 </p>
-                <p className="text-2xl text-gray-400">
+                <p className=" text-gray-400">
                   Odio euismod lacinia at quis. Amet purus gravida quis blandit
                   turpis.
                 </p>
@@ -94,18 +100,18 @@ function Collaboration() {
                     />
                   </svg>
                 </button>
-                <p className="text-3xl mt-5 mb-5">
+                <p className="text-2xl mt-5 mb-5">
                   Extend asset productive lifespan
                 </p>
-                <p className="text-2xl text-gray-400">
+                <p className=" text-gray-400">
                   Odio euismod lacinia at quis. Amet purus gravida quis blandit
                   turpis.
                 </p>
               </div>
-              <div className="mt-20 md:m-10">
+              <div className=" md:ml-10">
                 <button
                   type="button"
-                  className=" rounded-full p-5  flex text-blue-700 items-center justify-center"
+                  className=" rounded-full p-4  flex text-blue-700 items-center justify-center"
                   style={{ background: " #EAEFF8" }}
                 >
                   <svg
@@ -123,18 +129,18 @@ function Collaboration() {
                     />
                   </svg>
                 </button>
-                <p className="text-3xl mt-5 mb-5">
+                <p className="text-2xl mt-5 mb-5">
                   Eliminate demand and supply tailback
                 </p>
-                <p className="text-2xl text-gray-400">
+                <p className=" text-gray-400">
                   Odio euismod lacinia at quis. Amet purus gravida quis blandit
                   turpis.
                 </p>
               </div>
-              <div className="mt-20 md:m-10">
+              <div className=" md:ml-10">
                 <button
                   type="button"
-                  className=" rounded-full p-5  flex text-blue-700 items-center justify-center"
+                  className=" rounded-full p-4  flex text-blue-700 items-center justify-center"
                   style={{ background: " #EAEFF8" }}
                 >
                   <svg
@@ -152,16 +158,16 @@ function Collaboration() {
                     />
                   </svg>
                 </button>
-                <p className="text-3xl mt-5 mb-5">Reduce downtime and costs</p>
-                <p className="text-2xl text-gray-400">
+                <p className="text-2xl mt-5 mb-5">Reduce downtime and costs</p>
+                <p className=" text-gray-400">
                   Odio euismod lacinia at quis. Amet purus gravida quis blandit
                   turpis.
                 </p>
               </div>
-              <div className="mt-20 md:m-10">
+              <div className=" md:m-10">
                 <button
                   type="button"
-                  className=" rounded-full p-5  flex text-blue-700 items-center justify-center"
+                  className=" rounded-full p-4  flex text-blue-700 items-center justify-center"
                   style={{ background: " #EAEFF8" }}
                 >
                   <svg
@@ -179,10 +185,10 @@ function Collaboration() {
                     />
                   </svg>
                 </button>
-                <p className="text-3xl mt-5 mb-5">
+                <p className="text-2xl mt-5 mb-5">
                   Optimize maintenance process
                 </p>
-                <p className="text-2xl text-gray-400">
+                <p className=" text-gray-400">
                   Odio euismod lacinia at quis. Amet purus gravida quis blandit
                   turpis.
                 </p>
@@ -191,7 +197,7 @@ function Collaboration() {
           </div>
 
           <img
-            className=" md:w-1/2  h-auto max-w-full  rounded"
+            className=" md:w-1/2 object-none h-auto max-w-full   rounded"
             src={production2}
             alt="product"
             sizes=""
