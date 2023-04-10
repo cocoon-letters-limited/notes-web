39969d6bddb3ba34700efd36c64bedc3cc14eba8
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-04-10 16:00:52 +0100

    fix: scroll to top

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CheckoutSection from "./components/checkoutSection";
 
 const Checkout = () => {
+  useEffect(() => {
+    document.querySelector("body")?.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
   return (
     <div>
       <CheckoutSection />
