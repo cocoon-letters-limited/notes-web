b5c94006dbd492cff27895f675e587918641e8f1
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-04-10 12:10:04 +0100

    fix: update landing page with link

--- src/pages/home/pricing.tsx
+++ src/pages/home/pricing.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { BusinessData, EnterpriseData, facilityData } from "./priceData";
 
 function Pricing() {
@@ -20,14 +21,14 @@ function Pricing() {
               /month
             </span>
           </div>
-          <button
-            type="button"
+          <Link
+            to="/checkout?subscriptionType=facility"
             className="text-white bg-blue-600 hover:bg-blue-700  mt-10 focus:ring-4 
             focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium 
             rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
           >
             Choose plan
-          </button>
+          </Link>
           <ul className="space-y-5 my-7">
             {facilityData.map((data) => (
               <li key={data.id}>
@@ -103,12 +104,12 @@ function Pricing() {
               /month
             </span>
           </div>
-          <button
-            type="button"
+          <Link
+            to="/checkout?subscriptionType=business"
             className="text-blue-700 bg-white hover:bg-blue-700 focus:ring-4 mt-10 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
           >
             Choose plan
-          </button>
+          </Link>
           <ul className="space-y-5 my-7">
             {BusinessData.map(
               (dataFac: { id: number; title: string; list: Array<any> }) => (
