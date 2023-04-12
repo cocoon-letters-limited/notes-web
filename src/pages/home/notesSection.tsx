32d28b12c59ea30f60324a449bd4749a0a2fd371
muyi <eroosamuyi11451@gmail.com>
2023-04-12 15:21:31 +0100

    note

--- src/pages/home/notesSection.tsx
+++ src/pages/home/notesSection.tsx
@@ -36,15 +36,6 @@ const noteList: Array<{
     subtitle: "(Condition Monitoring Specialist)",
     description: `Ever since we implemented NoteOpX, we've seen a significant reduction in unexpected equipment downtime. The ability to identify and address potential failures before they occur has been a game-changer for our operations."`,
   },
-];
-
-const noteListOne: Array<{
-  id: number;
-  image: string;
-  username: string;
-  subtitle: string;
-  description: string;
-}> = [
   {
     id: 1,
     image: "string",
@@ -75,152 +66,123 @@ const noteListOne: Array<{
 ];
 
 function NotesSection() {
-  const [noteChange, setNoteChange] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+  const [startIndex, setStartIndex] = useState(0); // State to keep track of the start index of the visible elements
+  const array = noteList; // Sample array of elements
+  const visibleElements = 3; // Number of elements to show on screen at a time
 
+  const handleNextClick = () => {
+    // Increment the start index by 1, wrapping around to 0 if it exceeds the array length
+    setStartIndex((startIndex + 1) % array.length);
+    setIsSelected(true);
+  };
+
+  const handlePrevClick = () => {
+    // Decrement the start index by 1, wrapping around to the end of the array if it goes below 0
+    setStartIndex((startIndex - 1 + array.length) % array.length);
+    setIsSelected(false);
+  };
   return (
-    <div>
-      <div
-        className="md:pt-20 md:mt-20 md:pb-20 h-fix pb-10"
-        style={{ background: " #F1F5FF" }}
-      >
-        <div className=" mb:p-0 md:container md:mx-auto">
-          <div className="flex justify-between">
-            <p className="md:text-3xl  font-bold text-blue-600 md:p-0 p-5  text-sm ">
-              NOTE is loved by users
-            </p>
-            <div className="flex md:mt-0 mt-5 mr-5 md:mr-0">
-              <button
-                onClick={() => setNoteChange(false)}
-                type="button"
-                className={`
+    <div
+      className="md:pt-20 md:mt-20 md:pb-20 h-fix pb-10"
+      style={{ background: " #F1F5FF" }}
+    >
+      <div className=" mb:p-0 md:container md:mx-auto">
+        <div className="flex justify-between">
+          <p className="md:text-3xl  font-bold text-blue-600 md:p-0 p-5  text-sm ">
+            NOTE is loved by users
+          </p>
+          <div className="flex md:mt-0 mt-5 mr-5 md:mr-0">
+            <button
+              onClick={handlePrevClick}
+              type="button"
+              className={`
                rounded-full md:p-3 w-fit p-2 h-fit border-2 
                ${
-                 !noteChange
+                 !isSelected
                    ? "border-blue-700 text-blue-700 "
                    : "border-gray-400 text-gray-400 "
                }  
                flex  mr-5 
                items-center justify-center`}
-                style={{ background: " #EAEFF8" }}
+              style={{ background: " #EAEFF8" }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="fill-current md:w-6 md:h-6 w-4 h-4 "
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="fill-current md:w-6 md:h-6 w-4 h-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
-                  />
-                </svg>
-              </button>
-              <button
-                onClick={() => setNoteChange(true)}
-                type="button"
-                className={` 
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+                />
+              </svg>
+            </button>
+            <button
+              onClick={handleNextClick}
+              type="button"
+              className={` 
                 rounded-full md:p-3 w-fit p-2 h-fit border-2 
               ${
-                noteChange
+                isSelected
                   ? "border-blue-700 text-blue-700 "
                   : "border-gray-400 text-gray-400 "
               }  flex  
                items-center justify-center`}
-                style={{ background: " #EAEFF8" }}
+              style={{ background: " #EAEFF8" }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="fill-current  md:w-6 md:h-6 w-4 h-4 "
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="fill-current  md:w-6 md:h-6 w-4 h-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </button>
           </div>
-          {noteChange ? (
-            <div className="md:grid grid-cols-3 gap-10 md:mt-10 mt-5  ">
-              {noteList.map((note) => (
-                <div
-                  key={note.id}
-                  className="rounded-lg p-2  md:m-0 m-5 shadow-lg bg-white"
-                >
-                  <div className="md:px-3 md:py-6 py-3">
-                    <div className="flex ">
-                      <img
-                        className="rounded-full md:h-16 h-8 md:mt-0 mt-2 md:mr-0 mr-3 w-8 object-none md:w-16"
-                        src={production2}
-                        alt="product"
-                        sizes=""
-                      />
-                      <div className="md:ml-5 md:mt-3 mt-2">
-                        <p className="md:text-1xl font-semibold text-sm">
-                          {note.username}
-                        </p>
-                        <p className="text-slate-400 md:text-base text-sm">
-                          {note.subtitle}
-                        </p>
-                      </div>
-                    </div>
-                    <p className="text-gray-400 mt-5 mb-5 md:text-base text-sm">
-                      {note.description}
+        </div>
+
+        <div className="md:grid grid-cols-3 gap-10 md:mt-10 mt-5  ">
+          {array.slice(startIndex, startIndex + visibleElements).map((note) => (
+            <div
+              key={note.id}
+              className="rounded-lg p-2  md:m-0 m-5 shadow-lg bg-white"
+            >
+              <div className="md:px-3 md:py-6 py-3">
+                <div className="flex ">
+                  <img
+                    className="rounded-full md:h-10 h-8 md:mt-0 mt-2 md:mr-0 mr-3 w-8 object-none md:w-10"
+                    src={production2}
+                    alt="product"
+                    sizes=""
+                  />
+                  <div className="md:ml-5 md:mt-0 mt-2">
+                    <p className="md:text-1xl font-semibold text-sm">
+                      {note.username}
                     </p>
-                    <Rating
-                      name="half-rating"
-                      defaultValue={4.5}
-                      precision={0.5}
-                    />
+                    <p className="text-slate-400 md:text-base text-sm">
+                      {note.subtitle}
+                    </p>
                   </div>
                 </div>
-              ))}
+                <p className="text-gray-400 mt-3 mb-3 md:text-base text-xs">
+                  {note.description}
+                </p>
+                <Rating name="half-rating" defaultValue={4.5} precision={0.5} />
+              </div>
             </div>
-          ) : (
-            <div className="md:grid grid-cols-3 gap-10 md:mt-10 mt-5 ">
-              {noteListOne.map((note) => (
-                <div
-                  key={note.id}
-                  className="rounded-lg py-2 px-4 md:m-0 m-5 shadow-lg bg-white"
-                >
-                  <div className=" p-2 ">
-                    <div className="flex ">
-                      <img
-                        className="rounded-full md:h-16 h-8 md:mt-0 mt-2 md:mr-0 mr-3 w-8 object-none md:w-16"
-                        src={production2}
-                        alt="product"
-                        sizes=""
-                      />
-                      <div className="md:ml-5 md:mt-3 mt-2">
-                        <p className="md:text-1xl font-semibold text-sm">
-                          {note.username}
-                        </p>
-                        <p className="text-slate-400 md:text-base text-xs">
-                          {note.subtitle}
-                        </p>
-                      </div>
-                    </div>
-                    <p className="text-gray-400 mt-5 mb-5 md:text-base text-xs">
-                      {note.description}
-                    </p>
-                    <Rating
-                      name="half-rating"
-                      defaultValue={4.5}
-                      precision={0.5}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
