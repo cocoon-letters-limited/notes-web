d6c8979f36f43825d839447eaf7a937219c3fc5c
muyi <eroosamuyi11451@gmail.com>
2023-04-27 22:47:18 +0100

    fit

--- src/pages/workforce/components/Training/details.tsx
+++ src/pages/workforce/components/Training/details.tsx
@@ -10,9 +10,24 @@ const WorkForceTraningDetails: React.FC<ChildProps> = ({ onClick }) => {
       <div className="flex justify-between mt-2">
         <div>
           <button type="button" onClick={onClick}>
-            back icon
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
           </button>
-          <span className="font-semibold ml-3">Digital Marketing Strategy</span>
+          <span className="font-semibold ml-3 ">
+            Digital Marketing Strategy
+          </span>
         </div>
         <button
           type="button"
