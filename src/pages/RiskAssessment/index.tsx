5777c32c74a892cb6ac3b6bfbde9ea2e32efc52a
muyi <eroosamuyi11451@gmail.com>
2023-05-04 10:27:55 +0100

    text fix

--- src/pages/RiskAssessment/index.tsx
+++ src/pages/RiskAssessment/index.tsx
@@ -1,8 +1,47 @@
-import InputGroup from "components/elements/input";
 import React from "react";
-import ButtonGroup from "components/elements/button";
 import { PieChartCustom } from "./pieChartCustom";
 import CustomAccordion from "./customAccordion";
+
+const ButtonGroup = ({ title, buttonStyleType }: any) => {
+  return (
+    <div>
+      {buttonStyleType !== "outline" ? (
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          {title}
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+        >
+          {title}
+        </button>
+      )}
+    </div>
+  );
+};
+
+const InputGroup = ({ label }: any) => {
+  return (
+    <div>
+      <div>
+        <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          {label}
+        </p>
+        <input
+          type="text"
+          id="first_name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Softwebdigital"
+          required
+        />
+      </div>
+    </div>
+  );
+};
 
 function RiskAssessment() {
   return (
@@ -14,13 +53,13 @@ function RiskAssessment() {
           <p>Risk assessment No:</p>
         </div>
       </div>
-      <div className="grid grid-cols-8">
+      <div className="grid grid-cols-8 my-5">
         <div className="w-full col-span-2">
           <PieChartCustom />
         </div>
         <div className="col-span-6">
           <p className="my-3 font-semibold">Select possible hazards</p>
-          <div className="grid grid-cols-5 gap-3">
+          <div className="flex flex-wrap">
             <ButtonGroup title="Confined Space Entry" />
             <ButtonGroup buttonStyleType="outline" title="Awkward Access" />
             <ButtonGroup buttonStyleType="outline" title="Unguarded Opening" />
@@ -32,8 +71,9 @@ function RiskAssessment() {
             />
             <ButtonGroup title="Awkward Access" />
           </div>
-          <p className="my-3 font-semibold">Select possible hazards</p>
-          <div className="grid grid-flow-row-dense grid-cols-3 gap-3 grid-rows-3">
+          <p className="text-right my-3 text-[#054CD6]">More hazards...</p>
+          <p className="my-3 font-semibold">Select possible controls:</p>
+          <div className="flex flex-wrap">
             <ButtonGroup
               buttonStyleType="outline"
               title="Erect signs and barriers"
@@ -71,6 +111,7 @@ function RiskAssessment() {
               title="Ensure correct PPE to be worn"
             />
           </div>
+          <p className="text-right my-3 text-[#054CD6]">More controls...</p>
         </div>
       </div>
       <div className="mt-4">
