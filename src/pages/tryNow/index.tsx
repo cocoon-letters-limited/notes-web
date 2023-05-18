4ec9f696e2e9a9cd33e3cf485ab281987e4d2788
muyi <eroosamuyi11451@gmail.com>
2023-05-18 13:17:11 +0100

    try free

--- src/pages/tryNow/index.tsx
+++ src/pages/tryNow/index.tsx
@@ -1,9 +1,53 @@
-// import { facilityData } from "pages/home/priceData";
+import ButtonGroup from "components/elements/button";
+import Spinner from "components/elements/spinner";
+import { Formik, FormikErrors } from "formik";
+import { useAppDispatch } from "hooks/useAppDispatch";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { TryForFreeAPI } from "store/tryforfree/tryforfree.slice";
+
+type Props = {
+  name: string;
+  email: string;
+  password: string;
+  companyName: string;
+  companySize: number;
+  billingAddress: string;
+  reason: string;
+  industry: string;
+};
 
 function Trynow() {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  // const planDataList = useSelector((state: any) => state.planList);
+
+  const initialValues: Props = {
+    name: "",
+    email: "",
+    password: "",
+    companyName: "",
+    companySize: 0,
+    billingAddress: "",
+    reason: "",
+    industry: "",
+  };
+
+  const validate = (values: Props) => {
+    const errors: FormikErrors<Props> = {};
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.name) {
+      errors.name = "Required";
+    }
+
+    return errors;
+  };
+
   return (
     <div className="container mx-auto p-2">
       <button
@@ -27,111 +71,180 @@ function Trynow() {
         </svg>
         <p>back</p>
       </button>
-      <div className="mt-5">
-        <div className="grid md:grid-cols-2 md:gap-6 ">
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Full Name
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Company Name
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Company size
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Industry
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Billing Address
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="">
-            <p className="block mb-2 text-sm font-medium text-gray-900 ">
-              Company Email address
-            </p>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-          </div>
-        </div>
-        <div className="mt-3">
-          <p className="block mb-2 text-sm font-medium text-gray-900 ">
-            Tell us your reasons
-          </p>
-          <textarea
-            id="reason"
-            rows={4}
-            cols={50}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            required
-          />
-        </div>
-        <div className="flex mt-10 mb-10 flex-row-reverse ">
-          <button
-            type="button"
-            className="text-white w-fit bg-gray-200 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg
-             text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-400 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            className="text-white w-fit bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={async (values, actions) => {
+          actions.setSubmitting(true);
+          await dispatch(TryForFreeAPI(values));
+          actions.setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          handleSubmit,
+          isSubmitting,
+          handleChange,
+          touched,
+          errors,
+          handleBlur,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="mt-5">
+              <div className="grid md:grid-cols-2 md:gap-6 ">
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Full Name
+                  </p>
+                  <input
+                    type="text"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                    required
+                  />
+                </div>
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Email
+                  </p>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    required
+                  />
+                  <p className="text-red-500">
+                    {errors.email && touched.email && errors.email}
+                  </p>
+                </div>
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Password
+                  </p>
+                  <input
+                    type="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    required
+                  />
+                </div>
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Company size
+                  </p>
+                  <input
+                    type="number"
+                    id="companySize"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    name="companySize"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.companySize}
+                    required
+                  />
+                </div>
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Industry
+                  </p>
+                  <input
+                    type="industry"
+                    id="industry"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.industry}
+                    required
+                  />
+                </div>
+                <div className="">
+                  <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                    Company Name
+                  </p>
+                  <input
+                    type="companyName"
+                    id="companyName"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.companyName}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="mt-3">
+                <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                  Billing Address
+                </p>
+                <input
+                  type="billingAddress"
+                  id="billingAddress"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.billingAddress}
+                  required
+                />
+              </div>
+              <div className="mt-3">
+                <p className="block mb-2 text-sm font-medium text-gray-900 ">
+                  Tell us your reasons
+                </p>
+                <textarea
+                  id="reason"
+                  rows={4}
+                  cols={50}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.reason}
+                  required
+                />
+              </div>
+              <div className="grid-cols-9 gap-2 grid">
+                <button
+                  type="button"
+                  className=" mt-3 w-full bg-gray-200 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg
+             text-sm px-5 py-2.5 mr-3 dark:bg-gray-400 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Cancel
+                </button>
+                <ButtonGroup
+                  type="submit"
+                  title={isSubmitting ? <Spinner /> : " Request access"}
+                  disabled={isSubmitting}
+                />
+                {/* <button
+                  type="button"
+                  className="text-white w-fit bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg
              text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Request access
-          </button>
-        </div>
-      </div>
+                >
+                  Request access
+                </button> */}
+              </div>
+            </div>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
